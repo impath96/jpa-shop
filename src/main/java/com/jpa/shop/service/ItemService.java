23129e0b2f78f347a9327e4c011aa8ce91dcd6b0
impath96 <impath96@gmail.com>
2023-10-24 14:01:34 +0900

    #7 - 상품 리포지토리, 서비스 개발
    간단한 상품관련 기능 개발.
    
    * 상품 등록
    * 상품 목록 조회
    * 상품 단건 조회

---
+++ src/main/java/com/jpa/shop/service/ItemService.java
@@ -0,0 +1,30 @@
+package com.jpa.shop.service;
+
+import com.jpa.shop.domain.item.Item;
+import com.jpa.shop.repository.ItemRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findById(itemId);
+    }
+
+}
